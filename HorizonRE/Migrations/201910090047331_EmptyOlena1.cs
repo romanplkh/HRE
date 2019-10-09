83d34ee94a96608dbf85b8f6d573fe8899c2ea8e
Olena Stepanova <86sthelen@gmail.com>
2019-10-09 16:32:09 -0300

    seed DB with countries and provinces

---
+++ HorizonRE/Migrations/201910090047331_EmptyOlena1.cs
@@ -0,0 +1,16 @@
+namespace HorizonRE.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EmptyOlena1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
