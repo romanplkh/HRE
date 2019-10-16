e6828cc4142dc5e0c035b5e7d7477d16f8a760c8
Roman Pelikh <pelikh.roman@gmail.com>
2019-10-16 15:28:22 -0300

    Add Image Class

--- HorizonRE/Models/HorizonContext.cs
+++ HorizonRE/Models/HorizonContext.cs
@@ -14,6 +14,7 @@ namespace HorizonRE.Models
         public DbSet<Customer> Customers { get; set; }
         public DbSet<ProvinceCustomer> ProvincesCustomers { get; set; }
         public  DbSet<ProvinceEmployee> ProvincesEmployees { get; set; }
+        public  DbSet<ImageFile> Images { get; set; }
 
 
 
