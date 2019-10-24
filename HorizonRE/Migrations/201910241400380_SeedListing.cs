941a3c5b56543995591e44422c9d7aad9dadb896
Olena Stepanova <86sthelen@gmail.com>
2019-10-24 12:10:29 -0300

    listing details customer page

---
+++ HorizonRE/Migrations/201910241400380_SeedListing.cs
@@ -0,0 +1,19 @@
+namespace HorizonRE.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedListing : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Listings VALUES('24 West Street', 'Vancouver', 'British Columbia'," +
+                "'Canada', 'R3V8K6', '500', '3', 3, 500000, 1, '2019-10-23', '2020-01-23', 1," +
+                "1, 1)");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
