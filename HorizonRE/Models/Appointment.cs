aee9c7634d916ce690dfd2a6521ec51756f10bcb
Roman Pelikh <pelikh.roman@gmail.com>
2019-10-29 10:42:42 -0300

    Add appintments

---
+++ HorizonRE/Models/Appointment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HorizonRE.Models
+{
+    public class Appointment
+    {
+
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Comment { get; set; }
+
+        public int ListingId { get; set; }
+
+
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        public int EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
+
+
+
+
+
+    }
+}
--- HorizonRE/Models/HorizonContext.cs
+++ HorizonRE/Models/HorizonContext.cs
@@ -19,6 +19,8 @@ namespace HorizonRE.Models
         public  DbSet<AccessLevel> AccessLevels { get; set; }
         public  DbSet<Listing> Listings { get; set; }
         public  DbSet<CityArea> CityAreas { get; set; }
+        public  DbSet<Appointment> Appointments { get; set; }
+
 
 
 
