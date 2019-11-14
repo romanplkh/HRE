41ac82596c52c4112959b2705d61e0fd21bc9856
Olena Stepanova <86sthelen@gmail.com>
2019-11-13 22:32:28 -0400

    fixed search button on add app, added Roles model for auth, fixed auth conn string

---
+++ HorizonRE/Models/RoleName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HorizonRE.Models
+{
+    public class RoleName
+    {
+        public const string ADMIN = "Broker";
+        public const string AGENT = "Agent";
+        public const string MANAGER = "Manager";
+        public const string CUSTOMER = "Customer";
+    }
+}
